95fcc39c18050296ea6e3681a2da2a0d63237c20
Anton Samy <78489379+AntonSamy20@users.noreply.github.com>
2025-03-01 21:48:09 +0200

    Add files via upload
    The Roots of Quadratic Equation

---
+++ Quadratic Formula.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+void roots(double ,double ,double);
+int main(){
+    double a,b,c;
+    cout<<"The Equation must be aX^2 + bX + c = 0"<<endl;
+    cout<<"Enter the co-officient of X^2 : ";
+    cin>>a;
+    cout<<"Enter the co-officient of X: ";
+    cin>>b;
+    cout<<"Enter the constant value: ";
+    cin>>c;
+    cout<<"The roots of the equation is: ";
+    roots(a ,b ,c);
+    return 0;
+}
+void roots(double a,double b,double c){
+    double discriminant  = pow(b,2)-4*a*c;
+    double ro1 , ro2;
+    if (discriminant>=0){
+        cout << "Real solution" << endl;
+        ro1 = (-1 * b + sqrt(discriminant)) / (2 * a);
+        ro2 = (-1 * b - sqrt(discriminant)) / (2 * a);
+        cout << fixed << setprecision(4);
+        if (ro1 == ro2){
+            cout<<"The two roots are equal to: "<<ro1;
+        }else {
+            cout << "The first root is: " << ro1 << endl;
+            cout << "The second root is: " << ro2 << endl;
+        }
+    }else {
+        cout<<"imaginary solution"<<endl;
+        double real = -1*b/(2*a);
+        double imaginary = sqrt(-1*discriminant)/(2*a);
+        cout<<fixed<<setprecision(4);
+        cout<<"The first root is: "<<real<<" + "<<imaginary<<'j'<<endl;
+        cout<<"The second root is: "<<real<<" - "<<imaginary<<'j';
+    }
+}
